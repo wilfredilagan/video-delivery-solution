8e7bdb179efc3bf2bd2cbca94f52338de2a59782
wilfredilagan <wilfredilagan@gmail.com>
2019-12-16 14:46:15 -0500

    Update knex.js to remove username and password

--- knexfile.js
+++ knexfile.js
@@ -19,8 +19,8 @@ module.exports = {
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'postgres',
-    password: 'sandman',
+    user: '',
+    password: '',
     database: 'vds'
   },
     migrations: {
